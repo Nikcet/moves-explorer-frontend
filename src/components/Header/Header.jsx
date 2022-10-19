f0c2e375c8d3117353f812313b1ea2f1895d14ca
Nikcet <mstislav.kr@yandex.ru>
2022-10-20 01:11:09 +0300

    fix links at Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,15 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import './header.css';
 import React from 'react';
 import logo from '../../images/logo.png';
 
 function Header(props) {
   return (
     <div className="header">
-      <a href="/">
+      <a href="/" className='header__logo-link'>
         <img src={logo} alt='Логотип' className='header__logo' />
       </a>
       <div className="header__buttons">
-        <a href="#" className='header__button header__button-reg'>Регистриция</a>
-        <a href="#" className='header__button header__button-login'>Войти</a>
+        <a href="#valid" className='header__button header__button-reg'>Регистриция</a>
+        <a href="##valid" className='header__button header__button-login'>Войти</a>
       </div>
     </div>
   );
