38fd1f406ca96a8924e5ca06a7e7965293542209
Nikcet <mstislav.kr@yandex.ru>
2022-10-22 02:04:13 +0300

    add footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Footer(props) {
+  return (
+    <footer className="footer">
+      <div className="footer__main">
+        <div className="footer__whose">
+          <p className="footer__text">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+        </div>
+        <div className="footer__footer">
+          <p className="footer__copyright">&copy; 2022</p>
+          <div className="footer__links">
+            <a href="#/" className="footer__link">Яндекс.Практикум</a>
+            <a href="#/" className="footer__link">GitHub</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
