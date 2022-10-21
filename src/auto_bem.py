485d66f802f507110cb231f76cada77b7775b412
Nikcet <mstislav.kr@yandex.ru>
2022-10-22 00:37:47 +0300

    fix auto_bem problems

--- src/auto_bem.py
+++ src/auto_bem.py
@@ -2,106 +2,81 @@ from bs4 import BeautifulSoup
 import os
 import re
 
-# directory_name = 'src\components'
-indexed_file = 'AboutMe.jsx'
-file_extension = 'css'
+clear = lambda: os.system('cls')
 
 
 def create_path(dir_name):
+    os.chdir(os.path.dirname(__file__))
+    print('Создает директорию: ' + dir_name)
     if not os.path.isdir(dir_name):
-        if file_extension == 'css':
-            try:
-                os.mkdir(f'{os.getcwd()}\\{dir_name.capitalize()}')
-            except:
-                print('Такая директория уже есть.')
-        # elif file_extension == 'jsx':
-        #     os.mkdir(f'{os.getcwd()}\\{dir_name.capitalize()}')
-        else:
-            print('Нет такого расширения')
-            return
-
-        os.chdir(dir_name)
-        new_dir = os.getcwd()
-        os.chdir('../')
+        os.mkdir(dir_name)
     else:
-        os.chdir(dir_name)
-        new_dir = os.getcwd()
-        os.chdir('../')
-
-    return new_dir
+        print('Директория уже есть')
+    return os.path.isdir(dir_name)
 
 
-def read_file():
-    os.chdir('src\components\AboutMe')
-    print(os.getcwd())
-    with open(indexed_file) as file:
+def change_dir(dir_name):
+    print('Заходит в: ' + dir_name)
+    os.chdir(dir_name)
+    return os.getcwd()
+
+
+def read_file(indexed_file):
+    with open(f'{os.path.dirname(__file__)}\\{indexed_file}') as file:
         content = file.read()
         soup = BeautifulSoup(content, 'lxml')
     return soup
 
 
-def create_file(new_dir, name, extension):
-    with open(f'{new_dir}\\{name}.{extension}', 'w') as file:
-        if file_extension == 'css':
+def create_file(name):
+    print('Создает файл: ' + name)
+    if not os.path.exists(f'{name}.css'):
+        with open(f'{name}.css', 'a') as file:
             file.write(f'.{name} ' + '{}')
-        # elif file_extension == 'jsx':
-        #     file.write(
-        #         f'export default function {name.capitalize()}() ' + '{};')
+    else:
+        print('Такой файл уже существует.')
+
+
+def parse_file(ind_file):
+    elements = read_file(ind_file).body.find_all()
+    classes = set()
+    for el in elements:
+        if 'classname' in str(el):
+            found = re.findall(r'"(.*?)"', str(el))
+            classes.update(set(found))
+    return classes
+
+
+def main(indexed_file):
+    for class_name in parse_file(indexed_file):
+        if '__' in class_name:
+            split_name = class_name.split('__')
+            block_name = f'__{split_name[1]}'
+            create_path(block_name)
+            change_dir(block_name)
+            create_file(class_name)
+            change_dir('../')
         else:
-            print('Нет такого расширения')
-            return
-
-
-def main():
-    # components_dir = create_path(directory_name)
-    elements = read_file().body.find_all()
-
-    if '.jsx' in indexed_file:
-        classes = set()
-        for el in elements:
-            if 'classname' in str(el):
-                found = re.findall(r'"(.*?)"', str(el))
-                classes.update(set(found))
-        # print(classes)
-
-        for el in classes:
-            file_name = el
-            if '__' not in file_name:
-                # os.chdir(components_dir)
-                directory = create_path(file_name)
-                create_file(directory, file_name, file_extension)
-        
-        for el in classes:
-            file_name = el
-            if '__' in file_name:
-                if file_extension == 'jsx':
-                    continue
-                split_name = file_name.split('__')
-                os.chdir(split_name[0])
-                directory = create_path(f'__{split_name[1]}')
-                create_file(directory, file_name, file_extension)
-                os.chdir('../')
-            else:
-                # os.chdir(components_dir)
-                directory = create_path(file_name)
-                create_file(directory, file_name, file_extension)
-    # elif '.html' in indexed_file:
-    #     for el in elements:
-    #         file_name = el['class'][0]
-    #         if '__' in file_name:
-    #             if file_extension == 'jsx':
-    #                 continue
-    #             split_name = file_name.split('__')
-    #             os.chdir(split_name[0])
-    #             directory = create_path(f'__{split_name[1]}')
-    #             create_file(directory, file_name, file_extension)
-    #             os.chdir('../')
-    #         else:
-    #             os.chdir(components_dir)
-    #             directory = create_path(file_name)
-    #             create_file(directory, file_name, file_extension)
+            create_file(class_name)
+    print('Готово!')
 
 
 if __name__ == '__main__':
-    # print(os.getcwd())
-    main()
+    clear()
+    current_path = os.path.dirname(__file__)
+    print('Удали из целевого файла значения всех атрибутов, кроме className!')
+    input('Нажми Enter чтобы продолжить.')
+    clear()
+
+    print('Ищет файл с расширением jsx...')
+    indexed_file = ''
+    for item in os.listdir(current_path):
+        if '.jsx' in item:
+            indexed_file = item
+            print('Нашел ' + indexed_file)
+            break
+
+    if indexed_file:
+        main(indexed_file)
+    else:
+        print('Не нашел файл с расширением .jsx в данной директории.\nВыход.')
