387b31901f6404c7049e986b8c4588dccbe8ae31
Nikcet <mstislav.kr@yandex.ru>
2022-11-05 02:40:04 +0300

    fix links

--- src/components/Protfolio/Portfolio.jsx
+++ src/components/Protfolio/Portfolio.jsx
@@ -6,20 +6,26 @@ function Portfolio(props) {
     <section className="portfolio">
       <div className="portfolio__main">
         <h3 className="portfolio__header">Портфолио</h3>
-        <div className="portfolio__links">
-          <a href="#/" className="portfolio__link">
-            <div className="portfolio__link-block">Статичный сайт</div>
-            <img src={arrow} alt="" className="protfolio__link-arrow" />
-          </a>
-          <a href="#/" className="portfolio__link">
-            <div className="portfolio__link-block">Адаптивный сайт</div>
-            <img src={arrow} alt="" className="protfolio__link-arrow" />
-          </a>
-          <a href="#/" className="portfolio__link">
-            <div className="portfolio__link-block">Одностраничное приложение</div>
-            <img src={arrow} alt="" className="protfolio__link-arrow" />
-          </a>
-        </div>
+        <ul className="portfolio__links">
+          <li className="portfolio__item">
+            <a href="#/" className="portfolio__link">
+              <div className="portfolio__link-block">Статичный сайт</div>
+              <img src={arrow} alt="" className="protfolio__link-arrow" />
+            </a>
+          </li>
+          <li className="portfolio__item">
+            <a href="#/" className="portfolio__link">
+              <div className="portfolio__link-block">Адаптивный сайт</div>
+              <img src={arrow} alt="" className="protfolio__link-arrow" />
+            </a>
+          </li>
+          <li className="portfolio__item">
+            <a href="#/" className="portfolio__link">
+              <div className="portfolio__link-block">Одностраничное приложение</div>
+              <img src={arrow} alt="" className="protfolio__link-arrow" />
+            </a>
+          </li>
+        </ul>
       </div>
     </section>
   );
