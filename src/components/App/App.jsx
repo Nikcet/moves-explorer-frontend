10f8663153c6a380392dcae67bce18c32f52f3b8
Nikcet <mstislav.kr@yandex.ru>
2022-11-04 06:51:57 +0300

    add routes and components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,38 @@
+import React from 'react';
 import Header from '../Header/Header';
-import Main from '../Main/Main'
-// import Footer from '../Footer/Footer';
+import Main from '../Main/Main';
+import Footer from '../Footer/Footer';
+import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
+import ErrorPage from '../ErrorPage/ErrorPage';
+import Register from '../Register/Register';
+import Login from '../Login/Login';
+import { Route, Routes, Redirect, useHistory } from 'react-router-dom';
 
 function App() {
+
+  const [isLoggined, setIsLoggined] = React.useState(true);
+  // console.log(isLoggined);
+
+  React.useEffect(() => {
+    setIsLoggined(true);
+  }, [isLoggined]);
+
   return (
     <div className="App">
       <div className="page">
-        <Header />
-        <Main />
-        {/* <Footer /> */}
+        <Header isLoggined={isLoggined} />
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/movies' element={<Movies />} />
+          <Route path='/saved-movies' element={<SavedMovies />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/signin' element={<Login />} />
+          <Route path='/signup' element={<Register />} />
+          <Route path='*' element={<ErrorPage />}/>
+        </Routes>
+        <Footer />
       </div>
     </div>
   );
