41648ef922d3135156758a648e0e30f38d1a0cbe
Nikcet <mstislav.kr@yandex.ru>
2022-11-17 06:04:23 +0300

    config for apis

---
+++ src/utils/config.js
@@ -0,0 +1,5 @@
+const moviesApiUrl = 'https://api.nomoreparties.co/beatfilm-movies';
+const mainApiUrl = 'https://api.diploma.movies.nomoredomains.sbs'
+const localUrl = 'http://localhost:3000';
+
+export {moviesApiUrl, mainApiUrl, localUrl};
