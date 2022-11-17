7acef4302853adc032232a55dc7b5037fd8a6d01
Nikcet <mstislav.kr@yandex.ru>
2022-11-18 01:01:58 +0300

    try to save movies

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import Search from '../SearchForm/Search';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
-import Preloader from '../Preloader/Preloader';
+import { SearchSavedCards } from '../../contexts/SearchSavedCards';
 
 function SavedMovies(props) {
+
+  const [searchSavedCards, setSearchSavedCards] = React.useState([]);
+  const [cards, setCards] = React.useState([]);
+
+  // React.useEffect(() => {
+  //   // console.log(localStorage);
+  //   // for (let key in localStorage) {
+  //   //   setCards([...cards, JSON.parse(localStorage.getItem(key))])
+  //   // }
+  //   // setCards(localStorage.getItem());
+  // }, []);
+
+  function returnCards(cards) {
+    setSearchSavedCards(cards);
+  }
+
   return (
-    <section className="movies">
-      <Search />
-      <MoviesCardList />
-    </section>
+    <SearchSavedCards.Provider value={searchSavedCards} >
+      <section className="movies">
+        <Search onCards={returnCards} />
+        <MoviesCardList />
+      </section>
+    </SearchSavedCards.Provider>
   );
 }
 
