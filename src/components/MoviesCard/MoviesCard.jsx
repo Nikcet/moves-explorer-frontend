3e7211675dd386d64848d06a7f51f79b6c05b729
Nikcet <mstislav.kr@yandex.ru>
2022-11-21 03:16:03 +0300

    add removing films from saved

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,45 +1,47 @@
 import React from 'react';
 import icon from '../../images/icon__COLOR_invisible.svg';
+import deleteIcon from '../../images/icon__COLOR_icon-main-delete.svg';
+import {postMovie} from '../../utils/MainApi';
 // import imageSrc from '../../images/pic__COLOR_pic.png';
 
 
 function MoviesCard(props) {
   const [isSave, setIsSave] = React.useState(false);
-  // const [newListCards, setNewListCards] = React.useState([]);
   const URL = 'https://api.nomoreparties.co';
-  // console.log('рендер moviesCard')
-  // React.useEffect(() => {
-  //   if (localStorage.getItem('savedCards')) {
-  //     setNewListCards(localStorage.getItem('savedCards'));
-  //   } else {
-  //     localStorage.setItem('savedCards', []);
-  //   }
-  // }, []);
 
-  // React.useEffect(() => {
-  //   // console.log(newListCards);
-  //   // console.log(localStorage);
-    
-  // }, []);
-  
+  function deleteCardFilm() {
+    props.onDeleteCard(props.card.nameRU);
+  }
+
   function saveCardFilm(event) {
-    // setNewListCards([...newListCards, props.card]);
+    // console.log(props.card)
+    // postMovie(props.card)
+    // .then((data) => {console.log(data)})
+    // .catch((err) => {console.log(err)});
     localStorage.setItem(props.card.nameRU, JSON.stringify(props.card));
-    // console.log(newListCards);
     setIsSave(true);
   }
 
   return (
     <li className="card">
-      <div className="card__save-widget" >
-        {!isSave ?
-          <button type="button" className='card__save-button' onClick={saveCardFilm}>Сохранить</button>
+      {!props.isSaved ? <div className="card__save-widget" >
+        {!isSave ? <button type="button" className='card__save-button' onClick={saveCardFilm}>Сохранить</button>
           :
           <div className="card__save-icon-circle">
             <img src={icon} alt="Иконка: сохранено" className="card__save-icon" />
           </div>
         }
-      </div>
+      </div> :
+        <div className="card__delete-widget">
+          <button className="card__delete-button" type="button" onClick={deleteCardFilm}>
+            <div className="card__delete-icon-circle">
+              <img src={deleteIcon} alt="Иконка: удалить" className="card__delete-icon" />
+            </div>
+          </button>
+        </div>
+      }
+
+
       <a className='card__link' href={props.card.trailerLink} target="_blank" rel="noopener noreferrer">
         <img src={`${URL}/${props.card.image.url}`} alt={`Обложка фильма: ${props.card.nameRU}`} className="card__image" />
       </a>
