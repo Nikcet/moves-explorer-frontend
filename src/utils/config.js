40ec365597c5500767ea6e0b5ce0eda81fb43bf3
Nikcet <mstislav.kr@yandex.ru>
2022-12-05 23:39:11 +0300

    add new url to config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,6 @@
 const moviesApiUrl = 'https://api.nomoreparties.co/beatfilm-movies';
 const mainApiUrl = 'https://api.diploma.movies.nomoredomains.sbs'
+const imageStorageUrl = 'https://api.nomoreparties.co';
 const localUrl = 'http://localhost:3000';
 
-export {moviesApiUrl, mainApiUrl, localUrl};
+export {moviesApiUrl, mainApiUrl, localUrl, imageStorageUrl};
