04f63eff7bee57c99c4e54e825eecf4cf094bcf7
Nikcet <mstislav.kr@yandex.ru>
2023-01-11 11:14:48 +0300

    beauty fix

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
-const moviesApiUrl = 'https://api.nomoreparties.co/beatfilm-movies';
-const mainApiUrl = 'https://api.diploma.movies.nomoredomains.sbs'
-const imageStorageUrl = 'https://api.nomoreparties.co';
-const localUrl = 'http://localhost:3000';
+const MOVIES_API_URL = 'https://api.nomoreparties.co/beatfilm-movies';
+const MAIN_API_URL = 'https://api.diploma.movies.nomoredomains.sbs'
+const IMAGE_STORAGE_URL = 'https://api.nomoreparties.co';
+const LOCAL_URL = 'http://localhost:3000';
 
-export {moviesApiUrl, mainApiUrl, localUrl, imageStorageUrl};
+export {MOVIES_API_URL as moviesApiUrl, MAIN_API_URL as mainApiUrl, LOCAL_URL as localUrl, IMAGE_STORAGE_URL as imageStorageUrl};
