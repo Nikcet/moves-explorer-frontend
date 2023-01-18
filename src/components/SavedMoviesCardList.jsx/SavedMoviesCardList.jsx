a65b7e73e3fdf863501aeaeec10b0c5e167b2589
Nikcet <mstislav.kr@yandex.ru>
2023-01-18 06:49:30 +0300

    fix searching movies in saving movies

--- src/components/SavedMoviesCardList.jsx/SavedMoviesCardList.jsx
+++ src/components/SavedMoviesCardList.jsx/SavedMoviesCardList.jsx
@@ -2,79 +2,81 @@ import React from 'react';
 import MoviesCard from '../MoviesCard/MoviesCard';
 import Preloader from '../Preloader/Preloader';
 import More from '../More/More';
-import { SearchSavedCards } from '../../contexts/SearchSavedCards';
 
 function SavedMoviesCardList(props) {
-  const [isPreloader, setIsPreloader] = React.useState(true);
   const [visibleCards, setVisibleCards] = React.useState([]);
   const [amountOfCards, setAmountOfCards] = React.useState(9);
 
   const [amountOfNewCards, setAmountOfNewCards] = React.useState(3);
   const [windowWidth, setWindowWidth] = React.useState(window.screen.width);
 
-  const savedCards = React.useContext(SearchSavedCards);
 
-  // Держит прелоадер, пока список карточек пустой
   React.useEffect(() => {
-    if (savedCards.length > 0) {
-      setIsPreloader(false);
-      console.log('setVisibleCards(savedCards)', savedCards);
-      setVisibleCards(savedCards);
-    } else {
-      setIsPreloader(true);
+    if (localStorage.getItem('allSavedFilms')) {
+      console.log('Показывает все сохраненные фильмы.')
+      setVisibleCards(JSON.parse(localStorage.getItem('allSavedFilms')).slice(0, amountOfCards));
     }
-  }, [savedCards]);
+  }, []);
+
+  React.useEffect(() => {
+    if (localStorage.getItem('searchedSavedCards')) {
+      console.log('Показывает найденные карточки.');
+      console.log('savedCards: ', props.savedCards);
+      setVisibleCards(JSON.parse(localStorage.getItem('searchedSavedCards')));
+    }
+
+    // return () => localStorage.removeItem('searchedSavedCards');
+  }, [props.savedCards]);
 
   // Отслеживает размер окна браузера и если размер окна изменился, меняет количество подгружаемых карточек
   React.useEffect(() => {
-    window.onresize = () => {setWindowWidth(window.screen.width)};
-    
-    if (windowWidth < 1280) {
+    window.onresize = () => { setWindowWidth(window.screen.width) };
+
+    if (windowWidth > 1280) {
+      setAmountOfCards(12);
+      setAmountOfNewCards(3)
+    } else if (windowWidth < 1280 && windowWidth > 480) {
+      setAmountOfCards(8);
       setAmountOfNewCards(2);
-    } else {
-      setAmountOfNewCards(3);
+    } else if (windowWidth < 480 && windowWidth > 320) {
+      setAmountOfCards(5);
+      setAmountOfNewCards(2);
     }
-    return () => {window.onresize = null}
+    console.log('Экран');
+    return () => { window.onresize = null }
   }, [windowWidth])
 
-
   // Добавляет новые карточки в список для рендера
-  async function addNewCardsToRender() {
-    const totalAmount = amountOfCards + amountOfNewCards;
-    setAmountOfCards(totalAmount);
-    let allSavedFilms = [];
-    try {
-      allSavedFilms = JSON.parse(localStorage.getItem('allSavedFilms')).slice(0, totalAmount);
-      setVisibleCards(allSavedFilms);
-    } catch (err) {
-      console.log(err);
-    }
+  function addNewCardsToRender() {
+    console.log('Добавляет новые карточки в список для рендера ');
+    setVisibleCards(JSON.parse(localStorage.getItem('allSavedFilms')).slice(0, amountOfCards));
+    setAmountOfCards(amountOfCards + amountOfNewCards);
   }
-  
+
 
   if (visibleCards.length > 0) {
     return (
       <section className="movies-card-list">
-        {isPreloader ? <Preloader /> : <ul className="card-list">
+        {props.isPreloader ? <Preloader /> : <ul className="card-list">
           {visibleCards.map(item => {
-              return (
-                <MoviesCard
-                  key={item._id}
-                  card={item}
-                  onDeleteCard={props.onDeleteCard}
-                  isSaved={true}
-                />
-              )
+            return (
+              <MoviesCard
+                key={item._id}
+                card={item}
+                onDeleteCard={props.onDeleteCard}
+                isSaved={true}
+              />
+            )
           })}
         </ul>}
-        {savedCards > visibleCards && !isPreloader && <More setNewCards={addNewCardsToRender}/>}
+        {props.savedCards > visibleCards && !props.isPreloader && <More setNewCards={addNewCardsToRender} />}
       </section>
     );
   } else {
     return (
-    <section className="movies-card-list">
-      {isPreloader ? <Preloader /> : <p className='movies-card-list__plug'>Ничего не нашлось.</p>}
-    </section>
+      <section className="movies-card-list">
+        {props.isPreloader ? <Preloader /> : <p className='movies-card-list__plug'>Ничего не нашлось.</p>}
+      </section>
     )
   }
 }
