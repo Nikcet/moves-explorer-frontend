17c26d25ce077d18a6f2d3776e5c8ada25f15b27
Nikcet <mstislav.kr@yandex.ru>
2023-01-30 16:13:05 +0300

    fix menu bar for mobile screens

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import Navigation from '../Navigation/Navigation';
-// import logo from '../../images/logo.svg';
-import { Link } from 'react-router-dom';
 
 function Header(props) {
   return (
     <header className="header">
       <Navigation isLoggined={props.isLoggined}/>
-
-      {!props.isLoggined && <div className="header__buttons">
-        <Link to='/signup' className='header__button header__button-reg'>Регистрация</Link>
-        <Link to='/signin' className='header__button header__button-login'>Войти</Link>
-      </div>}
-
     </header>
   );
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,6 +5,15 @@ import { Link } from 'react-router-dom';
 
 function Navigation(props) {
 
+  const [isChecked, setIsChecked] = React.useState(false);
+
+  function checkedHandler(event) {
+    if (event.target.className !== 'navigation__box') {
+      setIsChecked(!isChecked);
+    }
+  }
+
+
   return (
     <nav className="navigation">
       <Link to='/' className='navigation__logo-link'>
@@ -12,15 +21,15 @@ function Navigation(props) {
       </Link>
       {props.isLoggined && <>
         <nav className="navigation__links">
-          <Link to='/movies' className="navigation__link">Фильмы</Link>
-          <Link to='/saved-movies' className="navigation__link">Сохранённые фильмы</Link>
+          <Link to='/movies' className="navigation__link" onClick={checkedHandler}>Фильмы</Link>
+          <Link to='/saved-movies' className="navigation__link" onClick={checkedHandler}>Сохранённые фильмы</Link>
         </nav>
       </>}
 
       {props.isLoggined && <>
         <div className="navigation__account-vidget navigation__account-vidget_mobile">
           <Link to='/profile' className="navigation__account-vidget">
-            <p className="navigation__account-name">Аккаунт</p>
+            <p className="navigation__account-name" onClick={checkedHandler}>Аккаунт</p>
             <div className="navigation__account-circle-icon">
               <img src={accountImage} alt='Иконка' className="navigation__account-icon" />
             </div>
@@ -28,25 +37,36 @@ function Navigation(props) {
         </div>
       </>}
 
+      {!props.isLoggined && <div className="navigation__buttons navigation__buttons_mobile">
+        <Link to='/signup' className='navigation__button' onClick={checkedHandler}>Регистрация</Link>
+        <Link to='/signin' className='navigation__button navigation__button-login' onClick={checkedHandler}>Войти</Link>
+      </div>}
+
 
       <div className="navigation__mobile">
-        <input id="menu__toggle" type="checkbox" className='navigation__toggle' />
+        <input id="menu__toggle" type="checkbox" className='navigation__toggle' checked={isChecked} onChange={() => { }} onClick={checkedHandler} />
         <label className="navigation__btn" htmlFor="menu__toggle">
           <span className='navigation__span'></span>
         </label>
 
         <nav className="navigation__box">
-          <ul className="navigation__list-links">
-            <li><Link to='/' className="navigation__link">Главная</Link></li>
-            <li><Link to='/movies' className="navigation__link">Фильмы</Link></li>
-            <li><Link to='/saved-movies' className="navigation__link">Сохранённые фильмы</Link></li>
-          </ul>
-          <Link to='/profile' className="navigation__account-vidget">
-            <p className="navigation__account-name">Аккаунт</p>
+          {props.isLoggined &&
+            <ul className="navigation__list-links">
+              <li><Link to='/' className="navigation__link" onClick={checkedHandler}>Главная</Link></li>
+              <li><Link to='/movies' className="navigation__link" onClick={checkedHandler}>Фильмы</Link></li>
+              <li><Link to='/saved-movies' className="navigation__link" onClick={checkedHandler}>Сохранённые фильмы</Link></li>
+            </ul>}
+          {props.isLoggined ? <Link to='/profile' className="navigation__account-vidget">
+            <p className="navigation__account-name" onClick={checkedHandler}>Аккаунт</p>
             <div className="navigation__account-circle-icon">
               <img src={accountImage} alt='Иконка' className="navigation__account-icon" />
             </div>
-          </Link>
+          </Link> :
+            <div className="navigation__mobile-auth">
+              <Link to='/signup' className='navigation__reg' onClick={checkedHandler}>Регистрация</Link>
+              <Link to='/signin' className='navigation__auth' onClick={checkedHandler}>Войти</Link>
+            </div>
+          }
         </nav>
       </div>
     </nav>
